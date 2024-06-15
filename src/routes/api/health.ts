47e127060d8094a78c9821fff61185542f5c9b9c
remorses <beats.by.morse@gmail.com>
2024-06-15 18:22:40 +0200

    fixes

--- src/routes/api/health.ts
+++
@@ -1,7 +0,0 @@
-import { LoaderFunctionArgs } from '@remix-run/node'
-
-
-export async function loader({ request }: LoaderFunctionArgs) {
-    // allow cors from anywhere
-    return Response.json({ ok: true })
-}
