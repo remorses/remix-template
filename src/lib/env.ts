6ecb095eee458a3133ecb5d518fb34099d0b12a7
remorses <beats.by.morse@gmail.com>
2024-06-16 11:14:00 +0200

    lib/env: add env

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -2,3 +2,12 @@ export const env = {
     //
     PUBLIC_URL: process.env.PUBLIC_URL,
 }
+
+// for (let k in env) {
+//     if (
+//         env[k] == null &&
+//         (typeof window === 'undefined' || k.includes('PUBLIC'))
+//     ) {
+//         throw new Error(`Missing env var ${k}`)
+//     }
+// }
