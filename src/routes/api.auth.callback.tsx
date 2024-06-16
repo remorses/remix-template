26765518507893c081fdfd7c807aa586d96595c0
remorses <beats.by.morse@gmail.com>
2024-06-16 12:38:07 +0200

    routes/api.auth.callback: magic link thing

--- src/routes/api.auth.callback.tsx
+++ src/routes/api.auth.callback.tsx
@@ -1,9 +1,11 @@
 import { redirect, type LoaderFunctionArgs } from '@remix-run/node'
 import { getSupabaseWithHeaders } from '../lib/supabase.server'
+import { notifyError } from '../lib/errors'
 
 export async function loader({ request }: LoaderFunctionArgs) {
     const url = new URL(request.url)
-    const code = url.searchParams.get('code')
+    const code = url.searchParams.get('code') || ''
+    const type = url.searchParams.get('type') || ''
     const next = url.searchParams.get('next') || '/x'
 
     if (code) {
@@ -14,10 +16,21 @@ export async function loader({ request }: LoaderFunctionArgs) {
         const { error } = await supabase.auth.exchangeCodeForSession(code)
 
         if (error) {
-            console.log('Error: auth callback ', error)
+            notifyError(error, 'Error exchanging code for session')
         }
         return redirect(next, { headers })
     }
+    if (type === 'magiclink') {
+        const { headers, supabase } = getSupabaseWithHeaders({
+            request,
+        })
+        const token_hash = url.searchParams.get('token_hash') || ''
+        const { error } = await supabase.auth.verifyOtp({ token_hash, type })
 
-    return redirect('/x')
+        if (error) {
+            notifyError(error, 'Error verifying OTP')
+        }
+        return redirect(next, { headers })
+    }
+    return redirect(next)
 }
